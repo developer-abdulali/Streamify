10958280372ec67013e79a2bb4fbb618cf781348
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-14 14:17:00 +0500

    added changes in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Required for correct routing
+  build: {
+    outDir: "dist", // Default is fine; update if you changed this
+  },
 });
