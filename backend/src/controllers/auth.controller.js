47d80d63e41414060276ce0d56130601d5df4594
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-15 17:09:57 +0500

    Update auth.controller.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -60,9 +60,9 @@ export async function signup(req, res) {
 
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      // httpOnly: true, // prevent XSS attacks,
-      // sameSite: "None", // prevent CSRF attacks
-      // secure: process.env.NODE_ENV === "production",
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "None", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -94,9 +94,9 @@ export async function login(req, res) {
 
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
-      // httpOnly: true, // prevent XSS attacks,
-      // sameSite: "None", // prevent CSRF attacks
-      // secure: process.env.NODE_ENV === "production",
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "None", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
     });
 
     res.status(200).json({ success: true, user });
