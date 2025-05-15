bad51b048e71f92f689ac8a15c0a4a80c1ee2c21
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-15 13:59:49 +0500

    added changes create new build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "/pages": "/src/pages", // Map absolute /pages to src/pages if used
+    },
+  },
+});
