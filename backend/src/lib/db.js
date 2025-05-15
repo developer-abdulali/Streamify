c0b81e483748482f14fe577fe34537db685e1f81
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-15 17:22:43 +0500

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,37 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log("Error in connecting to MongoDB", error);
+//     process.exit(1); // 1 means failure
+//   }
+// };
+
+// lib/db.js
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log("Error in connecting to MongoDB", error);
-    process.exit(1); // 1 means failure
+let cached = global.mongoose || { conn: null, promise: null };
+
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   }
-};
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  global.mongoose = cached;
+  return cached.conn;
+}
