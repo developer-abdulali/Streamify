3d72fac13ad2e18c667376bb9ae0083c7badebdf
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-15 16:29:37 +0500

    added changes in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -9,12 +9,14 @@ import chatRoutes from "./routes/chat.route.js";
 
 import { connectDB } from "./lib/db.js";
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://streamify-psi-pink.vercel.app"],
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     credentials: true, // allow frontend to send cookies
   })
 );
