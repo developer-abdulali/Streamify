572717df6bf9f6c4439f4d59b16ca60e9e8bbd88
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-15 13:24:07 +0500

    added changes in axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const BASE_URL = import.meta.env.VITE_SERVER_URL
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 export const axiosInstance = axios.create({
-  // baseURL: BASE_URL,
-  baseURL: "http://localhost:5000/api",
+  baseURL: SERVER_URL,
+  // baseURL: "http://localhost:5000/api",
   withCredentials: true, // send cookies with the request
 });
