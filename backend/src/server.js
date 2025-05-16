f20b42d31351070ff971fb2ff5074dcae29b63f8
Abdul Ali <103456792+developer-abdulali@users.noreply.github.com>
2025-05-16 14:53:21 +0500

    added changes in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,8 +16,8 @@ const PORT = process.env.PORT || 5000; // fallback port
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    // origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    // origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     credentials: true,
   })
 );
